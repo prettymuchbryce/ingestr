c10b86849f67299961d4b392e1073e064da799a8
prettymuchbryce <brycedneal@gmail.com>
2019-11-05 07:36:51 -0800

    Revert "Start to figure out how to fix receipts"
    This reverts commit 3119c167d31ee0af644975ce265807a6ae63a551.

--- utils.go
+++ utils.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"math/big"
 	"time"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -46,23 +45,14 @@ func unmarshalBlock(blockJSON string) (*types.Block, error) {
 	return types.NewBlockWithHeader(head).WithBody(txs, []*types.Header{}), nil
 }
 
-type ReceiptBlock struct {
-	Hash         common.Hash      `json:"hash"`
-	Transactions []*types.Receipt `json:"transactions"`
-	UncleHashes  []common.Hash    `json:"uncles"`
-	ParentHash   common.Hash      `json:"parentHash"       gencodec:"required"`
-	UncleHash    common.Hash      `json:"sha3Uncles"       gencodec:"required"`
-	Coinbase     common.Address   `json:"miner"            gencodec:"required"`
-	Root         common.Hash      `json:"stateRoot"        gencodec:"required"`
-	TxHash       common.Hash      `json:"transactionsRoot" gencodec:"required"`
-	ReceiptHash  common.Hash      `json:"receiptsRoot"     gencodec:"required"`
-	Bloom        types.Bloom      `json:"logsBloom"        gencodec:"required"`
-	Difficulty   *big.Int         `json:"difficulty"       gencodec:"required"`
-	Number       *big.Int         `json:"number"           gencodec:"required"`
-	GasLimit     uint64           `json:"gasLimit"         gencodec:"required"`
-	GasUsed      uint64           `json:"gasUsed"          gencodec:"required"`
-	Time         uint64           `json:"timestamp"        gencodec:"required"`
-	Extra        []byte           `json:"extraData"        gencodec:"required"`
-	MixDigest    common.Hash      `json:"mixHash"`
-	Nonce        types.BlockNonce `json:"nonce"`
+type rpcBlock struct {
+	Hash         common.Hash          `json:"hash"`
+	Transactions []*types.Transaction `json:"transactions"`
+	UncleHashes  []common.Hash        `json:"uncles"`
+}
+
+type txExtraInfo struct {
+	BlockNumber *string         `json:"blockNumber,omitempty"`
+	BlockHash   *common.Hash    `json:"blockHash,omitempty"`
+	From        *common.Address `json:"from,omitempty"`
 }
